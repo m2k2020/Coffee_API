7217588f4abf4d0a27f30b7df3a290b117083f47
Abdirahman Faghi Muriidi <maamaanka55@gmail.com>
2022-12-18 13:16:32 +0300

    submitted

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     url: 'm2k.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'S8OOH9yqnaJrbQHaSnRiXkTx4DumaFdE', // the client id generated for the auth0 app
-    callbackURL: 'http://127.0.0.1:4200', // the base url of the running ionic application. 
+    callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
   }
 };
